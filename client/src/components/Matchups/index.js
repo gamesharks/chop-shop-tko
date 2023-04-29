6ee1195454c4d1402fa8024f6639cdc206ddaf25
stickkman <stickkman@gmail.com>
2023-04-28 21:54:50 -0400

    finished styling matchups page, cleaned up code in all clientside matchup files, added images for vs and matchup container

--- client/src/components/Matchups/index.js
+++ client/src/components/Matchups/index.js
@@ -2,93 +2,17 @@
 
 import React from "react";
 // import styled from "styled-components";
+import VersesImage from '../../assets/images/vs04.jpg';
 import './matchups.css';
 import { useQuery } from '@apollo/client';
 import { GET_MATCHUPS } from '../../utils/queries';
 
-
-// const CardWrapper = styled.div`
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: space-between;
-//   align-items: center;
-//   width: 80vw;
-//   height: 100px;
-//   background-color: #f9f9f9;
-//   padding: 20px;
-//   border-radius: 5px;
-// `;
-
-// const FighterWrapper = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-  
-// `;
-
-// const FighterName = styled.h3`
-//   font-size: 20px;
-//   font-weight: bold;
-//   margin: 0;
-// `;
-
-// const Odds = styled.div`
-//   font-size: 20px;
-//   color: gray;
-//   margin-top: 30px;
-//   font-weight: bolder;
-// `;
-
-// const Versus = styled.span`
-//   font-size: 24px;
-//   margin: 0 10px;
-// `;
-
-// const PlaceholderFighter = styled.div`
-//   width: 100px;
-//   height: 100px;
-//   border: 2px solid #ccc;
-//   border-radius: 50%;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// `;
 
 function Matchups() {
   const { loading, error, data } = useQuery(GET_MATCHUPS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :{error.message}</p>;
-
-// const VersusCard = () => {
-//   return (
-//     <CardWrapper>
-//       <FighterWrapper>
-//         {/* v-fighters name -v */}
-//         <FighterName>{matchup.fightName}</FighterName>
-//         <PlaceholderFighter>
-//           {/* v-fighters image here-v */}
-//           <span>PF</span>
-//           {/* v-odds here-v  */}
-//         <Odds>+120</Odds>
-//         </PlaceholderFighter>
-//       </FighterWrapper>
-//       <Versus>vs</Versus>
-//       <FighterWrapper>
-//         <FighterName>Placeholder Fighter</FighterName>
-//         <PlaceholderFighter>
-//           <span>PF</span>
-//           <Odds>-120</Odds>
-//         </PlaceholderFighter>
-//       </FighterWrapper>
-//     </CardWrapper>
-//     )};
-// };
-
-
-
-
-
 
   return (
     <div className="matchups-container">
@@ -102,7 +26,9 @@ function Matchups() {
                 <div className="fighter-name">{matchup.fighters[0].name}</div>
                 <div className="fighter-odds">Odds: {matchup.fighters[0].odds}</div>
               </div>
-              <div className="versus">vs</div>
+              <div className="versus">
+                <img className="versusImage" src={VersesImage} alt="Verses Icon" />
+              </div>
               
               <div className="fighter-block">
                 <img src={`/images/${matchup.fighters[1].image}`} />
--- client/src/pages/Matchups.js
+++ client/src/pages/Matchups.js
@@ -4,39 +4,24 @@ import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
 import Matchups from '../components/Matchups/index';
-// import styled from "styled-components";
 
-
-// const Wrapper = styled.div`
-//   display: flex;
-
-//   justify-content: center;
-//   align-items: center;
-//   height: 80vh;
-// `;
-
-// const CardsWrapper = styled.div`
-  
-//   display: grid;
-//   grid-template-columns: repeat(1, 1fr);
-//   grid-gap: 50px;
-//   justify-content: space-evenly;
-//   margin: 25px;
-  
-// `;
 
 const MatchUps = () => {
   return (
-    <div className="container">
-      <h1>Upcoming Fights</h1>
-     
-      {/* {make component to show upcoming fights} */}
-     
-      <Matchups />
+    // can replace this with an actual image if needed later
+    <div className="" style={{
+      background: 'black',
+      width: '100vw',
+      minHeight: '100vh',
+    }}>
+
+      <div className="container">
+        <h1>Upcoming Fights</h1>
+          <Matchups />
     
-   
-      {/* make component for odds */}
+            {/* make component Bet/Wager Button */}
     </div>
+  </div>
   );
 };
 
